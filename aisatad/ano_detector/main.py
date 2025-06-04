815eb67f961ef49f6fb109cac1b3670b82b8ef8e
Juan WANG <150823876+JW1517@users.noreply.github.com>
2025-06-04 17:47:02 +0200

    juan0603

--- aisatad/ano_detector/main.py
+++ aisatad/ano_detector/main.py
@@ -7,10 +7,24 @@ from dateutil.parser import parse
 
 from aisatad.params import *
 from aisatad.function_files.data import get_data_with_cache #clean_data, load_data_to_bq
-# from aisatad.function_files.model import initialize_model, compile_model, train_model, evaluate_model
-# from aisatad.function_files.preprocessing import preprocess_features
-# from mlflow.registry import load_model, save_model, save_results
-# from mlflow.registry import mlflow_run, mlflow_transition_model
+# Model Imports
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier,StackingRegressor,StackingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+
+#scaler
+from sklearn.preprocessing import StandardScaler
+
+# pipiline
+from sklearn.pipeline import make_pipeline
+
+#model
+from sklearn.model_selection import cross_val_score,cross_validate
+
+# Metrics
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 
 
 # data():
@@ -51,19 +65,25 @@ df = get_data_with_cache(cache_path=data_bucket_cache_path)
 print("✅ preprocess() done \n")
 
 
-# def model():
-    #1. init()
-    #2. train()
-    #3. evaluate()
-    #4. pred()
+
+
+
+
+# #pipe_stacking = make_pipeline(preproc(df), model, memory=cachedir)
+# scoring = ['accuracy', 'f1', 'roc_auc']
+
+# #scores = cross_validate(model, X, y, cv=5, scoring=scoring, n_jobs=-1)
+# # Résultats
+# print("Accuracy (mean ± std): {:.4f} ± {:.4f}".format(scores['test_accuracy'].mean(), scores['test_accuracy'].std()))
+# print("F1-score (mean ± std): {:.4f} ± {:.4f}".format(scores['test_f1'].mean(), scores['test_f1'].std()))
+# print("ROC AUC (mean ± std): {:.4f} ± {:.4f}".format(scores['test_roc_auc'].mean(), scores['test_roc_auc'].std()))
+
+
+
+
 
 if __name__ == '__main__':
     #data()
     #preprocessing()
     #model()
-    print(BQ_DATASET)
-    print(LOCAL_DATA_PATH)
-    data_bucket_cache_path = Path(LOCAL_DATA_PATH).joinpath(f"{BQ_DATASET}.csv")
-    print(data_bucket_cache_path)
-    df = get_data_with_cache(cache_path=data_bucket_cache_path)
     pass
--- aisatad/function_files/model.py
+++ aisatad/function_files/model.py
@@ -91,3 +91,50 @@ def xgboost_model(X_train, y_train, X_test, y_test):
     roc = roc_auc_score(y_test, y_pred)
 
     return f"accuracy: {accuracy:.3f}, precision: {precision:.3f}, recall: {recall:.3f}, f1 : {f1:.3f} - roc : {roc:.3f}"
+
+
+
+# Juan
+df = pd.read_csv("/Users/juanborron/code/JW1517/AI_SAT_AD/raw_data/feature_data.csv")
+X = df.iloc[:, -18:]
+y = df['anomaly']
+
+# def model():
+    #1. init()
+    #2. train()
+    #3. evaluate()
+    #4. pred()
+
+# stacking
+
+lreg = LogisticRegression(class_weight="balanced")
+svc = SVC(kernel='linear',
+                C=1000,gamma=0.0001,
+                coef0=0,
+                class_weight="balanced")
+adabstc = AdaBoostClassifier(algorithm='SAMME',
+                               estimator=DecisionTreeClassifier(class_weight='balanced',
+                                                                max_depth=10,
+                                                                min_samples_leaf=2,
+                                                                min_samples_split=10))
+xgbc = XGBClassifier(
+        colsample_bytree=0.8,
+        eval_metric='logloss',
+        gamma=0.3,
+        learning_rate=0.2,
+        max_depth=5,
+        min_child_weight=1,
+        n_estimators=50,
+        use_label_encoder=False,
+        #scale_pos_weight=(y_train.value_counts()[0] / y_train.value_counts()[1]),
+        tree_method='hist'
+    )
+
+model = StackingClassifier(
+    estimators=[("lreg", lreg),("svc", svc), ("adabstc", adabstc), ("xgbc", xgbc)],
+    final_estimator=LogisticRegression(max_iter= 1000),
+    cv=5,
+    n_jobs=-1
+)
+
+
