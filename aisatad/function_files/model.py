8c7667beccf11cbaf6997496eee45a93cfa137e0
Juan WANG <150823876+JW1517@users.noreply.github.com>
2025-06-05 10:14:28 +0200

    juan0603_3empush

--- aisatad/function_files/model.py
+++ aisatad/function_files/model.py
@@ -1,17 +1,37 @@
-# Data Manipulation
+# base pour manipuler les data
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Model Imports
+#  Preprocessing
+from sklearn.preprocessing import StandardScaler
+
+
+# Models
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    StackingClassifier
+)
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 
 # Metrics
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score
+)
+# calcul time
+import time
+
+
+
+
+# def function of Arsene et Joss 0602
 
 def logistic_regression(X_train, y_train, X_test, y_test):
     model = LogisticRegression(class_weight="balanced")
@@ -94,47 +114,103 @@ def xgboost_model(X_train, y_train, X_test, y_test):
 
 
 
-# Juan
-df = pd.read_csv("/Users/juanborron/code/JW1517/AI_SAT_AD/raw_data/feature_data.csv")
-X = df.iloc[:, -18:]
-y = df['anomaly']
+# Juan0603:  def model_stacking() qui compare tous les models de mardi(par Arsene et Joss), plus model_stacking
+# et return les scores of comparason dans un df_results_stacking
 
-# def model():
-    #1. init()
-    #2. train()
-    #3. evaluate()
-    #4. pred()
+def model_stacking(X_train, y_train, X_test,y_test):
+    """
+    1. lister les models déjà fait gridsearch
+    2. stacking les models
+    3. entrainer les models plus celui de stacking, et mis les results de scores dans un dataframe
+    """
 
-# stacking
+    """
+    1. lister les models déjà fait gridsearch
+    """
 
-lreg = LogisticRegression(class_weight="balanced")
-svc = SVC(kernel='linear',
-                C=1000,gamma=0.0001,
-                coef0=0,
-                class_weight="balanced")
-adabstc = AdaBoostClassifier(algorithm='SAMME',
-                               estimator=DecisionTreeClassifier(class_weight='balanced',
-                                                                max_depth=10,
-                                                                min_samples_leaf=2,
-                                                                min_samples_split=10))
-xgbc = XGBClassifier(
-        colsample_bytree=0.8,
-        eval_metric='logloss',
-        gamma=0.3,
-        learning_rate=0.2,
-        max_depth=5,
-        min_child_weight=1,
-        n_estimators=50,
-        use_label_encoder=False,
-        #scale_pos_weight=(y_train.value_counts()[0] / y_train.value_counts()[1]),
-        tree_method='hist'
+    #supervised model à stacking: LogisticRegression
+    lreg = LogisticRegression(class_weight="balanced")
+    #supervised model à stacking: SVC
+    svc = SVC(kernel='linear',
+                    C=1000,gamma=0.0001,
+                    coef0=0,
+                    class_weight="balanced",
+                    probability=True)
+    #supervised model à stacking: AdaBoostClassifier
+    adabstc = AdaBoostClassifier(estimator=DecisionTreeClassifier(class_weight='balanced',
+                                                                    max_depth=10,
+                                                                    min_samples_leaf=2,
+                                                                    min_samples_split=10)
+                                )
+    #supervised model à stacking: XGBClassifier
+    xgbc = XGBClassifier(
+            colsample_bytree=0.8,
+            eval_metric='logloss',
+            gamma=0.3,
+            learning_rate=0.2,
+            max_depth=5,
+            min_child_weight=1,
+            n_estimators=50,
+            #scale_pos_weight=(y_train.value_counts()[0] / y_train.value_counts()[1]),
+            tree_method='hist'
+        )
+
+    """
+    2. stacking les models
+    """
+
+    mdl_stacking = StackingClassifier(
+        estimators=[("lreg", lreg),("svc", svc), ("adabstc", adabstc), ("xgbc", xgbc)],
+        final_estimator=LogisticRegression(max_iter= 1000),
+        cv=5,
+        n_jobs=-1
     )
 
-model = StackingClassifier(
-    estimators=[("lreg", lreg),("svc", svc), ("adabstc", adabstc), ("xgbc", xgbc)],
-    final_estimator=LogisticRegression(max_iter= 1000),
-    cv=5,
-    n_jobs=-1
-)
+    """
+    3. entrainer les models plus celui de stacking, et mis les results de scores dans un dataframe
+    """
 
+    # dictionary of models
 
+    model_lists = {
+            "lreg": lreg,
+            "svc": svc,
+            "adabstc": adabstc,
+            "xgbc": xgbc,
+            "mdl_stack" :mdl_stacking
+    }
+
+    results = []
+    #for loop iterate model to get scores
+    for model_nm, model_t in model_lists.items():
+
+        start_time = time.time()
+        #train model
+        model_t.fit(X_train, y_train)
+        # model predict
+        y_pred = model_t.predict(X_test)
+        #calcul les metrics
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        roc = roc_auc_score(y_test, y_pred)
+        #print les metrics
+        print(f" results for model {model_nm}:  [accuracy: {accuracy:.3f}, precision: {precision:.3f}, recall: {recall:.3f}, f1 : {f1:.3f} - roc : {roc:.3f}]")
+        #calul time
+        cal_time = time.time()- start_time
+        # save results dans une list
+        results.append({
+            "model_nm": model_nm,
+            "y_pred": y_pred,
+            "accuracy": accuracy,
+            "precision":precision,
+            "recall":recall,
+            "f1":f1,
+            "roc":roc,
+            "elapsed_time": cal_time
+            })
+    #transformer en pd
+    df_results_stacking = pd.DataFrame(results)
+
+    return df_results_stacking
