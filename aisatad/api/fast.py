7990f3007c47c1b61bd5d0855ab54fcd9b5745c1
Juan WANG <150823876+JW1517@users.noreply.github.com>
2025-06-09 12:26:05 +0200

    juan060912H"

--- aisatad/api/fast.py
+++ aisatad/api/fast.py
@@ -1,5 +1,5 @@
 from aisatad.params import *
-from aisatad.function_files.preprocessor import preprocess, gen_data_scaled
+from aisatad.function_files.preprocessor import api_preprocess_pipeline
 from aisatad.function_files.registry import load_model
 import pandas as pd
 
@@ -10,33 +10,16 @@ from fastapi import FastAPI
 # a fast api
 app = FastAPI()
 app.state.model = load_model()
-app.state.scaler = load_scaler()
+#app.state.scaler = load_scaler()
 
 
 @app.get("/predict")
-def predict(dico):
+def predict(data: dict) :
 
-    df = pd.DataFrame(dico)
+    df = pd.DataFrame(data)
 
-    df_preproc = gen_data_scaled(df, app.state.scaler)
+    #X_scaled = api_preprocess_pipeline(df, app.state.scaler)
 
-    y_pred = app.state.model.predict(df_preproc)
+    #y_pred = app.state.model.predict(X_scaled)
 
-    return {"anomaly": y_pred}
-
-
-    # X_pred = pd.DataFrame({
-    #     "pickup_datetime": [pd.Timestamp(pickup_datetime, tz='UTC')],
-    #     "pickup_longitude": [pickup_longitude],
-    #     "pickup_latitude": [pickup_latitude],
-    #     "dropoff_longitude": [dropoff_longitude],
-    #     "dropoff_latitude": [dropoff_latitude],
-    #     "passenger_count": [passenger_count]
-    # })
-
-    # X_pred_preprocessed = preprocess_features(X_pred)
-
-    # result = app.state.model.predict(X_pred_preprocessed)
-
-    # return {"fare": float(result[0][0])}
-
+    return {"anomaly": 0}
