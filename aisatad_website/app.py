208bddffd7bd53d98ff0a907b88fb39ff18ca7cb
Yves Sporri <yves.sporri@gmail.com>
2025-06-11 17:24:41 +0200

    app de ouf

--- aisatad_website/app.py
+++ aisatad_website/app.py
@@ -1,52 +1,49 @@
 import streamlit as st
-import datetime
 import requests
 import pandas as pd
-import io
 import ast
 import matplotlib.pyplot as plt
 
 
-'''
-# aisatad front
-'''
-
 def plot_seg_ax_st(ax, df_raw, segment):
     df_filtered = df_raw[df_raw["segment"] == segment]
-    ax.scatter(df_filtered["timestamp"], df_filtered["value"])
-    ax.set_title(f"Segment {segment}\n | Channel {df_filtered['channel'].iloc[0]}")
-    ax.set_xlabel("Timestamp")
-    ax.set_ylabel("Value")
+    ax.scatter(df_filtered["timestamp"], df_filtered["value"], s=1)
+    ax.set_xticks([])
+    ax.set_yticks([])
 
 
-st.title("Prédiction via FastAPI")
 
-uploaded_file = st.file_uploader("Uploader un fichier CSV", type=["csv"])
+st.title("AI SAT AD")
+
+uploaded_file = st.file_uploader("Uploader des mesures (format CSV)", type=["csv"])
 base_url = "https://aisatad-img-204615645613.europe-west1.run.app/"
 
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.write("Aperçu du CSV :", df.head())
 
     nb_seg_plot = 5
-    fig, ax = plt.subplots(nb_seg_plot, figsize=(12, 17))
+    fig, ax = plt.subplots(2,3, figsize=(6, 3))
     segments = df["segment"].unique()[:nb_seg_plot]
-    for i, segment in enumerate(segments):
-        plot_seg_ax_st(ax[i], df, segment)
-        
+
+    for ax, segment in zip(ax.flat, segments) :
+        plot_seg_ax_st(ax, df, segment)
+
     plt.tight_layout()
     st.pyplot(fig)
 
-    if st.button("Envoyer pour prédiction"):
+    if st.button("Détecter les anomalies"):
         url_post= base_url + "/predict"
         res = ast.literal_eval(df.to_json())
         response = requests.post(url_post, json = res)
 
         if response.status_code == 200:
             data = response.json()
-            st.write("Réponse brute de l’API :", data)
-
+            for key, value in data.items() :
+                if value == 0 :
+                    st.write("Segment numéro", key, 'OK')
+                else :
+                    st.write("Segment numéro", key, 'ANOMALIE')
         else:
             st.error(f"Erreur lors de l'appel à l'API : {response.status_code}")
             st.text(response.text)
